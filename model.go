4a33852df8c482f5dce57568bc85c62113d33d30
onlyangel <jose@ngel.es>
2020-11-10 04:58:46 -0600

    Migrationg to support gorn connectors

--- model.go
+++ model.go
@@ -2,6 +2,8 @@ package model_go
 
 import (
 	"fmt"
+	"github.com/go-errors/errors"
+	"log"
 )
 
 var conectornamess []string
@@ -20,4 +22,34 @@ func Open(conectorname string) (ModelConnector, error) {
 		return nil, fmt.Errorf("Conector doesn't exists")
 	}
 	return val, nil
+}
+
+/*
+set defaults
+*/
+var default_modelname string
+var default_conectionstring string
+
+func SetDefaultConectors(model, conectionstring string) {
+	// TODO VERIFY VALIDITY OF MODEL NAMES
+	default_conectionstring = conectionstring
+	default_modelname = model
+}
+
+func OpenDefault() (*ModelConnector, error) {
+
+	log.Printf("Conectores Disponibles \n%+v\nmodel: '%s'\nconnectg string: %s\n", conectors, default_modelname, default_conectionstring)
+
+	val, ok := conectors[default_modelname]
+	if !ok {
+		return nil, fmt.Errorf("Conector doesn't exists")
+	}
+
+	if default_conectionstring == "" {
+		return nil, errors.New("Not default setted")
+	}
+	val.SetConnectionString(default_conectionstring)
+
+	log.Printf("Returging val: '%+v'", val)
+	return &val, nil
 }
